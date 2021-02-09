589711eb1d8f639a611e8a41201497590b4a84b1
Sarita Patil <74887580+saritaPro@users.noreply.github.com>
2021-02-09 18:59:51 +0530

    Add files via upload

---
+++ for.py
@@ -0,0 +1,9 @@
+#/usr/bin/python
+
+for letter in "python":           #first example
+	print "current letter:",letter
+
+fruits =["banana","apple","mango"]
+for fruit in fruits:             #second example
+	print"current fruit:",fruit
+	print "Good bye!"
