f1c471a5cb46b47465dd852952439b4b061abddf
Sarita Patil <74887580+saritaPro@users.noreply.github.com>
2021-02-09 19:05:07 +0530

    Add files via upload
    Relational operators code in Python

---
+++ relational.py
@@ -0,0 +1,36 @@
+a=10
+b=20
+c=0
+
+print"A=",a
+print"B=",b
+
+if(a==b):
+	print"statement is true:"
+else:
+	print"statement is false:"
+
+if(a<>b):
+        print"statement is true:"
+else:
+        print"statement is false:"
+
+if(a<b):
+        print"statement is true:"
+else:
+        print"statement is false:"
+
+if(a>b):
+        print"statement is true:"
+else:
+        print"statement is false:"
+
+if(a<=b):
+        print"statement is true:"
+else:
+        print"statement is false:"
+
+if(a>=b):
+        print"statement is true:"
+else:
+        print"statement is false:"
