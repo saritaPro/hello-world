87f0039223cec07bef53159f83f993bf7224c9b8
Sarita Patil <74887580+saritaPro@users.noreply.github.com>
2021-02-09 18:58:27 +0530

    Add files via upload
    Increment code in Python

---
+++ increment.py
@@ -0,0 +1,34 @@
+#this program is for increment and decrement operation
+a=20
+b=40
+
+# a is incremented by value 1 and then printed 
+a+=1
+print("increment of a",a)
+a+=1
+print("increment of a",a)
+
+
+# b is incremented by value 1 and then printed 
+b+=1
+print("increment of b",b)
+b+=1
+print("increment of b",b)
+
+# a is decremented by value 1 and then printed
+a-=1
+print("decrement of a",a)
+
+# a is decremented by value 2 and then printed
+a-=2
+print("decrement of a",a)
+
+# b is decremented by value 1 and then printed
+a-=1
+print("decrement of b",b)
+
+# b is decremented by value 2 and then printed
+a-=2
+print("decrement of b",b)
+
+
