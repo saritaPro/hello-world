1bd69d15163bc5e56ab7df24895baa2039d4c387
Sarita Patil <74887580+saritaPro@users.noreply.github.com>
2021-02-09 19:04:15 +0530

    Add files via upload
    Arithmatic operators code in Python

---
+++ arithmatic.py
@@ -0,0 +1,24 @@
+#this program is for arithmatic operation
+a=88
+b=37
+c=0
+
+c=a+b
+print("addition of c is",a+b)
+
+
+c=a-b
+print("substraction of c is",a-b)
+
+
+c=a*b
+print("multiplication of c is",a*b)
+
+
+c=a/b
+print("division of c is",a/b)
+
+
+c=a%b
+print("mode of c is",a%b)
+
